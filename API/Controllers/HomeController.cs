db0ece63e0351ec85f511f2d3536c0be1cf6fa85
Cashe Rumsey <rumseycashe@gmail.com>
2018-03-13 16:23:41 -0600

    Created API project

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// API for Location control
+    /// </summary>
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("api/test", Name = "Test")]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult Test(string message)
+        {
+            return Ok(message);
+        }
+    }
+}
