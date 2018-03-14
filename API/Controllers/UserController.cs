d876916b5f5a1e615f227ef81ac34874d8cd61a5
Cashe Rumsey <rumseycashe@gmail.com>
2018-03-13 22:30:23 -0600

    Setup database, API, and Login/Register user

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Configuration;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// API for Location control
+    /// </summary>
+    public class UserController : ApiController
+    {
+        DeskShellEntities db = DatabaseEntityCreator.CreateDB();
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("api/register", Name = "Register")]
+        [ResponseType(typeof(IHttpActionResult))]
+        public IHttpActionResult Register(UserAuthentication user)
+        {
+            if (user == null || string.IsNullOrWhiteSpace(user.Username) || string.IsNullOrWhiteSpace(user.Password))
+                return BadRequest("Invalid User!");
+            else if (db.Users.Where(x => x.Username.ToLower().Equals(user.Username)).FirstOrDefault() != null)
+                return BadRequest("Username already exists!");
+
+            try
+            {
+                var encryptedPassword = Crypto.EncryptStringAES(user.Username, ConfigurationManager.AppSettings["SharedKey"]);
+                db.Users.Add(new User()
+                {
+                    Username = user.Username,
+                    Password = encryptedPassword,
+                    Created = DateTime.UtcNow,
+                    LastLogin = null
+                });
+                db.SaveChanges();
+
+                return Ok($"Succesfully Created New User {user.Username}.");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Failed to create new user account!");
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("api/login", Name = "Login")]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult Login(UserAuthentication user)
+        {
+            if (user == null || string.IsNullOrWhiteSpace(user.Username) || string.IsNullOrWhiteSpace(user.Password))
+                return BadRequest("Invalid Login");
+
+            try
+            {
+                var decryptedPassword = Crypto.DecryptStringAES(user.Username, ConfigurationManager.AppSettings["SharedKey"]);
+                var existingUser = db.Users.Where(x => x.Username.ToLower().Equals(user.Username.ToLower()) && x.Password.Equals(decryptedPassword)).FirstOrDefault();
+
+                if (existingUser == null)
+                    return BadRequest("Invalid Login");
+
+                return Ok("Success!");
+            }
+            catch (Exception)
+            {
+                return BadRequest("Failed to Login");
+            }
+        }
+    }
+}
